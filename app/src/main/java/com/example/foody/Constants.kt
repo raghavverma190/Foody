c45402446a7b4f82a7a194c797967173f0d89e97
Raghav Verma <raghav.verma190@gmail.com>
2024-03-21 01:27:50 +0530

    Added constants file and refactoring of UI

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.foody
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "d2e1f88a4bde431ebecaf8b5d20cd5c9"
+
+    }
+
+}
--- app/src/main/java/com/example/foody/ui/fragments/favorites/FavouriteRecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/favorites/FavouriteRecipesFragment.kt
@@ -1,21 +1,19 @@
-package com.example.foody
+package com.example.foody.ui.fragments.favorites
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_recipes.view.*
+import com.example.foody.R
 
-class RecipesFragment : Fragment() {
+class FavouriteRecipesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
-        view.recyclerview.showShimmer()
-        return view
+        return inflater.inflate(R.layout.fragment_favourite_recipes, container, false)
     }
 
 }
